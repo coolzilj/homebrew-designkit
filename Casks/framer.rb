1df0507f56c637ab7c2a50a7cbf92305c79b6bfb
Jin Liu <coolziljin@gmail.com>
2016-07-26 13:16:29 +0800

    add framer studio

---
+++ Casks/framer.rb
@@ -0,0 +1,8 @@
+cask 'framer' do
+  version '65'
+  sha256 '9c63b8db9d743e25d3f77707d49918f06eb1a00a7e4686bd65660a61ea8df01a'
+  homepage 'http://framerjs.com/'
+  url 'http://localhost:1111/framer.app.zip'
+  name 'Framer'
+  app 'Framer.app'
+end
